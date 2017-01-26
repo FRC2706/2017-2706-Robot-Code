d0cacb374c6e0ffb7e31686cd4f5746a0729b795
Connor Adams <cnnr252@gmail.com>
2017-01-25 19:09:43 -0500

    Mostly Documentation

--- src/org/usfirst/frc/team2706/robot/commands/autonomous/automodes/CenterToLaunch.java
+++ src/org/usfirst/frc/team2706/robot/commands/autonomous/automodes/CenterToLaunch.java
@@ -7,7 +7,17 @@ import org.usfirst.frc.team2706.robot.commands.autonomous.plays.DrivePlaceGear;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class CenterToLaunch extends CommandGroup {
-
+	/**
+	 * Gets center gear, then heads over to the other team's launch area
+	 * 
+	 * @param rightSide Are we going right or left?
+	 * @param speed What speeed are we traveling at?
+	 * @param gearDistance How far away is the gear from our starting position
+	 * @param reverseDistance How far back are we going after placing the gear?
+	 * @param turnAmount How much are we turning to not hit the airship?
+	 * @param sideDistance How far are we going while angled?
+	 * @param toLaunchPad How far is it to the launch pad from our location?
+	 */
 	public CenterToLaunch(boolean rightSide, double speed, double gearDistance, double reverseDistance, double turnAmount, double sideDistance, double toLaunchPad) {
 		this.addSequential(new DrivePlaceGear(speed,gearDistance,reverseDistance));
 		this.addSequential(new QuickRotate(rightSide ? turnAmount : -turnAmount));
--- src/org/usfirst/frc/team2706/robot/commands/autonomous/plays/DrivePlaceGear.java
+++ src/org/usfirst/frc/team2706/robot/commands/autonomous/plays/DrivePlaceGear.java
@@ -6,6 +6,13 @@ import org.usfirst.frc.team2706.robot.commands.mechanismcontrol.GearMechanism;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DrivePlaceGear extends CommandGroup {
+	/**
+	 * Place the gear and then back up after
+	 * 
+	 * @param speed What speed are we placing the gear at?
+	 * @param distance How far away is the gear?
+	 * @param reverseDistance How far in reverse do we want to go after placing the gear?
+	 */
 	public DrivePlaceGear(double speed, double distance, double reverseDistance) {
 		//Adds a movement one after another instead of at the same time
 		this.addSequential(new StraightDriveWithEncoders(speed,distance,25,5));
