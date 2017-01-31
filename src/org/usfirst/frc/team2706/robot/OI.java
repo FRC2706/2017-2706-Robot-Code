a8b925bc2b3514486cb28ce53af006dcfe3b6e8f
Connor Adams <cnnr252@gmail.com>
2017-01-30 20:04:59 -0500

    Added a cancelWhenReleased function

--- src/org/usfirst/frc/team2706/robot/OI.java
+++ src/org/usfirst/frc/team2706/robot/OI.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team2706.robot;
 import org.usfirst.frc.team2706.robot.commands.autonomous.movements.StraightDriveWithEncoders;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -30,9 +29,11 @@ public class OI {
 			
 			// Joystick for controlling the mechanisms of the robot
 			controlStick = new Joystick(1);
-			JoystickButton LB = new JoystickButton(driverStick,5);
+			
+			EJoystickButton LB = new EJoystickButton(driverStick,5);
 			StraightDriveWithEncoders sdwe = new StraightDriveWithEncoders(0.75,-8/12.0,5,1);
 			LB.whenPressed(sdwe);
+			LB.cancelWhenReleased(sdwe);
 	    }
 }
 
