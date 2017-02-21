985cc2653e799bf6503ca7b6511179e8f109d9e4
Kevin Lam <kevlam@gmail.com>
2017-02-21 11:32:38 -0500

    Update Camera.java

--- src/org/usfirst/frc/team2706/robot/subsystems/Camera.java
+++ src/org/usfirst/frc/team2706/robot/subsystems/Camera.java
@@ -14,16 +14,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * Does everything java related to the rPI camera, servo controllers have been deleted.
  */
 public class Camera extends Subsystem {
+    // TODO we should probably consolidate these two IP addresses
     public static final String RPI_IP = "10.27.6.240";
     TrackerBox2 trackerbox = new TrackerBox2(RPI_IP);
     public static final String CAMERA_IP = "10.27.6.240";
     public boolean PRINT_STUFF = false;
     public String RPi_addr;
     public final int visionDataPort = 1182;
+    
     @Override
     protected void initDefaultCommand() {
-        // TODO Auto-generated method stub
-        
     }
 
    public Camera(String ip) {
@@ -32,24 +32,13 @@ public class Camera extends Subsystem {
     }
 
    public void GetTargets() {
-   
-           ArrayList<TrackerBox2.TargetObject> targets = trackerbox.getVisionData();
-              if (targets!=null){
-
+       ArrayList<TrackerBox2.TargetObject> targets = trackerbox.getVisionData();
+              
+       if (targets != null) {
            System.out.println("I found "+targets.size()+" targets.");
            for(TrackerBox2.TargetObject target : targets)
                System.out.println("\tI found: "+target.toString());
-
            System.out.println();
-              }
+       }
    }
-   /*public float GetBoundingArea() {
-        return cachedTarget.boundingArea;
-    }*/
-
-   /* public boolean HasTarget() {
-        return cachedTarget == null ? false : true;
-    }*/
 }
-
-
